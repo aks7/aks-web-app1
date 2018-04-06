984f048af9d812815abdd3f738540074a4e7f5f3
aks7 <a4amit6@gmail.com>
2018-04-07 01:41:20 +0530

    added project directory

---
+++ src/main/java/com/aks/web/akswebapp1/AksWebApp1Application.java
@@ -0,0 +1,12 @@
+package com.aks.web.akswebapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AksWebApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AksWebApp1Application.class, args);
+	}
+}
